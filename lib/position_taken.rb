7771489390b8a4892191ea538aa4b9cc17e76c9c
Theresa <thetheresagarcia@gmail.com>
2019-11-25 18:54:00 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,4 +2,11 @@
 
 def position_taken?(board, index)
 
+    taken = nil
+    if (board[index] == " " || board[index] == "" || board[index] == nil)
+       taken = false
+    else
+       taken = true
+    end      
+
 end  
